cbbb87eabfa6cf69e99ccaea1ac66f94c286f49d
Richard Barnitz <richardbarnitz@gmail.com>
2024-02-05 00:07:57 -0800

    Airline submit working and logging to DOM

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,55 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import storeInstance from '../..';
 
 function App() {
+  const [airlineName, setAirlineName] = useState('');
+  const airline = useSelector((state) => state.airline);
+
+  const dispatch = useDispatch();
+
+  function submit(event) {
+    event.preventDefault();
+
+    dispatch({
+      type: 'AIRLINE_NAME',
+      payload: airlineName,
+    });
+
+    console.log(storeInstance.getState());
+
+    console.log(airlineName);
+  }
+
+  function handleChange(event) {
+    setAirlineName(event);
+  }
+
   return (
     <div>
       <h1>Redux Airport</h1>
-      <input placeholder="Airline Name" />
-      <button>Add Airline</button>
-      <table>{/* Airlines should be listed here */}</table>
+      <form onSubmit={submit}>
+        <input
+          placeholder="Airline Name"
+          value={airlineName}
+          onChange={(event) => handleChange(event.target.value)}
+        />
+        <button type="submit">ADD AIRLINE!</button>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Airline Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {airline.map((airline, index) => (
+            <tr key={index}>
+              <td>{airline}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -6,14 +6,31 @@ import App from './components/App/App';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore, combineReducers } from 'redux';
 
-/** TODO: Add REDUCERS */
+//import logger:
+import logger from 'redux-logger';
 
+/** TODO: Add REDUCERS */
+const airline = (state = [], action) => {
+  // Log the action
+  if (action.type === 'AIRLINE_NAME') {
+    console.log(`The element was ${action.payload}`);
+  }
+  return [...state, action.payload];
+};
+
+const planeCount = (state = 0, action) => {
+  // Log the action
+  console.log(`Hey!!! I'm a reducer y'all!!!`, action);
+
+  return state;
+};
 /** TODO: Create store */
 const storeInstance = createStore(
   combineReducers({
     airline,
     planeCount,
-  })
+  }),
+  applyMiddleware(logger)
 );
 
 // Be sure to add the Provider! Just wrap App with it. Don't copy and paste from lecture, that will cause issues.
@@ -25,3 +42,5 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+export default storeInstance; // Export the store
