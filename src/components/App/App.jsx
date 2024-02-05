54f95a49be74ee974f59c3cb3ea56829c29e1069
Richard Barnitz <richardbarnitz@gmail.com>
2024-02-04 21:43:24 -0800

    added dependencies

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 
 function App() {
-
   return (
     <div>
       <h1>Redux Airport</h1>
-      <input placeholder='Airline Name' />
+      <input placeholder="Airline Name" />
       <button>Add Airline</button>
       <table>{/* Airlines should be listed here */}</table>
     </div>
--- src/index.jsx
+++ src/index.jsx
@@ -3,19 +3,25 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 
 /** TODO: import REDUX **/
-
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
 
 /** TODO: Add REDUCERS */
 
-
 /** TODO: Create store */
-
-
+const storeInstance = createStore(
+  combineReducers({
+    airline,
+    planeCount,
+  })
+);
 
 // Be sure to add the Provider! Just wrap App with it. Don't copy and paste from lecture, that will cause issues.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={storeInstance}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
